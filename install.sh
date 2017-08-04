331b4baa4b023cd1c355459a157aa76e347c739e
forker <thanks@david.davis>
2017-08-04 18:22:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,20 @@
 # To install, just run the following command. You must have ruby, rake, git,
 # and zsh installed.
 #
-# curl -L https://raw.github.com/daviddavis/dotfiles/master/install.sh | sh
+# curl -L https://raw.githubusercontent.com/kimhin/daviddavis-dotfiles/master/install.sh | sh
 #
 
 # install oh-my-zsh
 if [ -e "/bin/zsh" ]
 then
-  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  if [ ! -d "~/.oh-my-zsh" ]
+  then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  fi
 fi
 
 # get the dotfiles
-git clone --recursive https://github.com/daviddavis/dotfiles.git ~/.dotfiles
+git clone --recursive https://github.com/kimhin/daviddavis-dotfiles.git ~/.dotfiles
 
 # install vim plugins
 cd ~/.dotfiles
